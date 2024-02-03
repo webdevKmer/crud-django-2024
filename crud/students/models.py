3c46cfba4c3914c61b357b997b08859be22d4ba9
webdevKmer <webdevKmer@gmail.com>
2024-02-03 01:18:01 +0000

    create student model and make migrations

--- crud/students/models.py
+++ crud/students/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Grad (models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Lesson (models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Student (models.Model):
+    name = models.CharField(max_length=50)
+    grad = models.ForeignKey("Grad", verbose_name='grades', on_delete=models.CASCADE)
+    lesson = models.ManyToManyField("lesson", verbose_name='lessons')
+
+    def __str__(self):
+        return self.name
